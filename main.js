082d325befd9a7a57fd249414a6afc8eb41feb37
xytham <madhubasyal11@gmail.com>
2022-01-31 18:26:08 +0545

    hahh

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+//alert('please dont use Website is in mentenence');
+//  console.warn('this is warning for you');
+ // console.error('There is error found');
+
+var number3 = 100;
+var number4 = 29;
+console.log(number3 + number4);
+
+
+ 
