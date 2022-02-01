0c44b4eb9d82b8cf6305583010f76e872aab8394
xytham <madhubasyal11@gmail.com>
2022-02-01 19:26:21 +0545

    hgd

--- main.js
+++ main.js
@@ -2,9 +2,57 @@
 //  console.warn('this is warning for you');
  // console.error('There is error found');
 
-var number3 = 100;
-var number4 = 29;
-console.log(number3 + number4);
+//var number3 = 100;
+//var number4 = 29;
+//console.log(number3 + number4);
 
 
+// var A = true;
+ //var b = false;
+// console.log(A);
+// console.log(b);
  
+
+// var x = null;
+ 
+// console.log(x);
+// var n = undefined;
+// console.log(n);
+ 
+ //var A = 20;
+ //var B = 40;
+ 
+ //o = 12;
+ //l = 29;
+ 
+//console.log('The sum of o/l',l*o);
+// console.log('The sum of A+B',A+B);
+// console.log('The sum of A/B',A/B);
+// console.log('The sum of A-B',A-B);
+ //console.log('The sum of A*B',A*B);
+/// c = b;
+ //true or false 
+//var o = 5;
+//var p = 9;
+//console.log(p < o);
+ //console.log(o < p);
+ 
+ 
+// var age = 10;
+// if ('age > 8')
+ 
+//console.log('you are not a kid');
+
+//else{
+//////  console.log('you  are a kid')
+//console.log('real time of nepal')
+import React from 'react
+
+const BasicForm (O {
+return
+
+div>
+
+export default BasicForm
+
+
